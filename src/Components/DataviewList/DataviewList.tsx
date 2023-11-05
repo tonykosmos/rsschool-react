3f0da700533a8950e2d2c937e39597b47ba06c77
tonykosmos <a.kosmovskiy@gmail.com>
2023-11-06 00:35:15 +0300

    refactor: code refactoring

--- src/Components/DataviewList/DataviewList.tsx
+++ src/Components/DataviewList/DataviewList.tsx
@@ -9,19 +9,17 @@ function DataviewList(props: DataviewListProps) {
   return (
     <div className={classes.flexContainer}>
       <div>
-        <div>
-          {props.data.map((item: Person) => (
-            <DataviewItem
-              key={item.name}
-              name={item.name}
-              height={item.height}
-              mass={item.mass}
-              hair_color={item.hair_color}
-              birth_year={item.birth_year}
-              url={item.url}
-            />
-          ))}
-        </div>
+        {props.data.map((item: Person) => (
+          <DataviewItem
+            key={item.name}
+            name={item.name}
+            height={item.height}
+            mass={item.mass}
+            hair_color={item.hair_color}
+            birth_year={item.birth_year}
+            url={item.url}
+          />
+        ))}
       </div>
       <div className={classes.detailsPage}>
         <Outlet />
