7d33ef94a5a49c2dc407dd9efe8c13e0bb48c3a2
tonykosmos <a.kosmovskiy@gmail.com>
2023-11-20 01:30:45 +0300

    refactor: remove commented code

--- src/Components/DataviewList/DataviewList.test.tsx
+++ src/Components/DataviewList/DataviewList.test.tsx
@@ -1,36 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, vi } from 'vitest';
+import { describe, it } from 'vitest';
 import DataviewList from './DataviewList';
-import { Context } from '../../context/context';
-import { mockDataviewListData, mockSearchString } from '../../mocks/mockData';
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from '../../store';
 import { Provider } from 'react-redux';
-import { setCardsData } from '../../store/searchSlice';
 
 describe('DataviewList tests', () => {
-  // it('Should show 3 items', async () => {
-  //   render(
-  //     <MemoryRouter initialEntries={['/']}>
-  //       <Provider store={store}>
-  //         <DataviewList />
-  //       </Provider>
-  //     </MemoryRouter>
-  //   );
-
-  //   store.dispatch(
-  //     setCardsData({
-  //       count: 3,
-  //       results: mockDataviewListData,
-  //       next: null,
-  //       previous: null,
-  //     })
-  //   );
-
-  //   const items = await screen.findAllByTestId('dataview-item');
-  //   expect(items.length).toBe(3);
-  // });
-
   it('Should show load spinnew while loading data', async () => {
     render(
       <BrowserRouter>
